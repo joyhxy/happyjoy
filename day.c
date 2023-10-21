1cc0192e2d2580f4a0aaed7b354868e1a2e1807e
joyhxy <148559253+joyhxy@users.noreply.github.com>
2023-10-21 17:26:50 +0800

    Add files via upload

---
+++ day.c
@@ -0,0 +1,30 @@
+int main()
+{
+    int day = 0;
+	scanf("%d",&day);
+    switch(day)
+    {
+	case 1:
+    printf("星期1\n");
+    break;
+	case 2:
+	break;
+    printf("星期2\n");
+	break;
+	case 3:
+    printf("星期3\n");
+	break;
+	case 4:
+    printf("星期4\n");
+	break;
+	case 5:
+    printf("星期5\n");
+	break;
+	case 6:
+    printf("星期6\n");
+	break;
+	case 7:
+    printf("星期7\n");
+    }
+	return 0;
+}
