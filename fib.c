f15c85fe5cf0f533df3efa798155463e86d75ad4
joyhxy <148559253+joyhxy@users.noreply.github.com>
2023-10-25 22:23:25 +0800

    Add files via upload

---
+++ fib.c
@@ -0,0 +1,22 @@
+int fib(int n)
+{
+	int a=1;
+	int b=1;
+	int c=1;
+	while(n>2)
+	{
+		c=a+b;
+		a=b;
+		b=c;
+		n--;
+	}
+	return c;
+}
+int main()
+{
+	int n=0;
+	scanf("%d",&n);
+	int i=fib(n);
+	printf("%d\n",i);
+	return 0;
+}
---
+++ sizeof.c
@@ -0,0 +1,10 @@
+int main()
+{
+	char arr1[]="abc";
+	char arr2[]={'a','b','c'};
+	printf("%d\n",sizeof(arr1));
+	printf("%d\n",sizeof(arr2));
+	printf("%d\n",strlen(arr1));
+	printf("%d\n",strlen(arr2));
+	return 0;
+ } 
