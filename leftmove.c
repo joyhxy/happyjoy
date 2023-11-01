5a26b53828a2c8de6839132fa00084e2cb92b34e
joyhxy <148559253+joyhxy@users.noreply.github.com>
2023-11-01 18:21:50 +0800

    Add files via upload

---
+++ leftmove.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+	int a=8;
+	int b=a<<1;
+	printf(" b=%d\n",b); 
+	return 0;
+	
+}
---
+++ rightmove.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	int a=-2;
+	int b=a>>1;
+	printf("b=%d\n",b);
+	printf("a=%d\n",a);
+	return 0;
+	
+}
