2f05722ce15a9803c333abe9268b533d04fe702d
joyhxy <148559253+joyhxy@users.noreply.github.com>
2023-11-09 22:29:33 +0800

    Add files via upload

---
+++ shijian.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	int b;
+	printf("This program computes moduli. ");
+	printf("Enter an integer to serve as the second operand:");
+	sancf("%d",&b);
+	printf("Now enter the first operand:");
+	scanf("%d",&a);
+	//printf("%d%%%d is %d",a,b,a%b);
+	return 0;
+ } 
