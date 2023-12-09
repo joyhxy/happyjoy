b1a745d26c307bfd35a91553982547db710fb460
joyhxy <148559253+joyhxy@users.noreply.github.com>
2023-12-09 21:09:09 +0800

    Add files via upload

--- 2.2.c
+++ 2.2.c
@@ -3,9 +3,16 @@ double mean(double,double);
 int main()
 {
 	double a,b;
-    printf("Please enter two numbers.\n");
+	printf("Please enter two numbers(!=0).\n");
 	scanf("%lf %lf",&a,&b);	
-	printf("the harmonic average is %lf",mean(a,b));
+	while(a!=0&&b!=0)
+	{
+	printf("the harmonic average is %lf.\n",mean(a,b));
+	printf("Please enter two numbers(!=0).\n");
+	scanf("%lf %lf",&a,&b);	
+    }
+	printf("error number,done.\n");
+	
 	return 0;
 }
 double mean(double a,double b)
